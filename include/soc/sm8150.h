b1ba0a6b199811aab5f596bbce77213dc4c49973
halal-beef <78730004+halal-beef@users.noreply.github.com>
2024-06-15 15:23:42 +0100

    [DNM] Initial vayu support

---
+++ include/soc/sm8150.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2024, halal-beef <78730004+halal-beef@users.noreply.github.com>
+ */
+
+#ifndef SM8150_H_	/* Include guard */
+#define SM8150_H_
+
+#endif // SM8150_H_
--- main/main.c
+++ main/main.c
@@ -8,7 +8,14 @@
 
 void main(void* dt, void* kernel) {
 	/* Initialize SoC and Board specific peripherals/quirks */
-
+    *(int*) (0x0 + 0x8) = 0;
+    char* base = (char*)CONFIG_FRAMEBUFFER_BASE;
+    for (int i = 0; i < 1080*2400*4; i += 4) {
+        base[i] = 255;      // Blue component
+        base[i + 1] = 255;  // Green component
+        base[i + 2] = 255;  // Red component
+        base[i + 3] = 255;        // Full opacity
+    }
 	/* TODO: Find a better way to make this more universal (since devices like arm64 Samsung Galaxies enable FB after soc_init) */
 #ifdef CONFIG_SIMPLE_FB
 	clean_fb((char*)CONFIG_FRAMEBUFFER_BASE, CONFIG_FRAMEBUFFER_WIDTH, CONFIG_FRAMEBUFFER_HEIGHT, CONFIG_FRAMEBUFFER_STRIDE);
@@ -23,7 +30,7 @@ void main(void* dt, void* kernel) {
 	printk("Booting linux...");
 
 	memcpy((void*)CONFIG_PAYLOAD_ENTRY, kernel, (unsigned long) &kernel_size);
-	load_kernel(dt, 0, 0, 0, (void*)CONFIG_PAYLOAD_ENTRY);
+//	load_kernel(dt, 0, 0, 0, (void*)CONFIG_PAYLOAD_ENTRY);
 
 	/* We shouldn't get there */
 	while(1) {}
