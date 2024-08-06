d6d29404b55c1b6a51bcd9f190531fe323f850c7
halal-beef <78730004+halal-beef@users.noreply.github.com>
2024-08-06 23:08:52 +0100

    libfdt POC

--- main/main.c
+++ main/main.c
@@ -5,6 +5,19 @@
 
 #include <main.h>
 #include <string.h>
+#include <lib/libfdt.h>
+
+unsigned long __stack_chk_guard;
+void __stack_chk_guard_setup(void)
+{
+     __stack_chk_guard = 0xBAAAAAAD;//provide some magic numbers
+}
+
+void __stack_chk_fail(void)                         
+{
+ /* Error message */                                 
+}// will be called when guard variable is corrupted 
+
 
 void main(void* dt, void* kernel) {
 	/* Initialize SoC and Board specific peripherals/quirks */
@@ -22,8 +35,12 @@ void main(void* dt, void* kernel) {
 	/* Copy kernel to memory and boot  */
 	printk("Booting linux...");
 
-	memcpy((void*)CONFIG_PAYLOAD_ENTRY, kernel, (unsigned long) &kernel_size);
-	load_kernel(dt, 0, 0, 0, (void*)CONFIG_PAYLOAD_ENTRY);
+        const void *fdtP = (const void *)(0x8A080000);
+
+	if (fdt_check_header(fdtP) == 0) printk("ITS AN FDT JIM!");
+
+//	memcpy((void*)CONFIG_PAYLOAD_ENTRY, kernel, (unsigned long) &kernel_size);
+//	load_kernel(dt, 0, 0, 0, (void*)CONFIG_PAYLOAD_ENTRY);
 
 	/* We shouldn't get there */
 	while(1) {}
